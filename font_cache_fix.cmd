ea4e4c61eaf81e26469ef16cd97438ee78c51fc6
Elad Karako <eladkarako@users.noreply.github.com>
2017-08-23 16:09:45 +0300

    Update font_cache_fix.cmd

--- font_cache_fix.cmd
+++ font_cache_fix.cmd
@@ -4,17 +4,20 @@
 ::  and re-enables them. A reboot is needed.  You may want to keep
 ::  "Windows Presentation Foundation Font Cache 3.0.0.0"  disabled
 ::  if you have any issues with higher than normal CPU on a legacy
-::  Dot.Net applications.      /Created By Elad Karako. July 2017.
+::  Dot.Net applications.    /Created By Elad Karako. August 2017.
 echo.
 echo Font-Cache Fix.
 
-  sc  config  "FontCache" start=disabled               2>nul >nul
-  sc  config  "FontCache3.0.0.0" start=disabled        2>nul >nul
+  sc  config  "FontCache"          start=disabled      2>nul >nul
+  sc  config  "FontCache3.0.0.0"   start=disabled      2>nul >nul
+  sc  config  "WPFFontCache_v0400" start=disabled      2>nul >nul
     
   sc  stop    "FontCache"                              2>nul >nul
   net stop    "FontCache" /y                           2>nul >nul
   sc  stop    "FontCache3.0.0.0"                       2>nul >nul
   net stop    "FontCache3.0.0.0" /y                    2>nul >nul
+  sc  stop    "WPFFontCache_v0400"                     2>nul >nul
+  net stop    "WPFFontCache_v0400" /y                  2>nul >nul
     
   del /f /q   "%SystemRoot%\System32\FNTCACHE.DAT"     2>nul >nul
   del /f /q   "%SystemRoot%\SysWOW64\FNTCACHE.DAT"     2>nul >nul
@@ -22,8 +25,9 @@ echo Font-Cache Fix.
   del /f /q   "%SystemRoot%\System32\StaticCache.dat"  2>nul >nul
   del /f /q   "%SystemRoot%\SysWOW64\StaticCache.dat"  2>nul >nul
 
-  sc config   "FontCache"        start=auto            2>nul >nul
-::sc config   "FontCache3.0.0.0" start=auto            2>nul >nul
+  sc  config  "FontCache"          start=auto          2>nul >nul
+::sc  config  "FontCache3.0.0.0"   start=auto          2>nul >nul
+::sc  config  "WPFFontCache_v0400" start=auto          2>nul >nul
 
 echo Done.
 echo.
